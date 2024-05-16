5d6b6f182c3065365e75b27a2f2b3d5cda452fd5
andrei-barysenka <andrei.barysenka@gmail.com>
2024-05-16 17:49:41 +0200

    update Munich credentials

--- src/main/java/genepi/imputationserver/util/DefaultPreferenceStore.java
+++ src/main/java/genepi/imputationserver/util/DefaultPreferenceStore.java
@@ -82,8 +82,8 @@ public class DefaultPreferenceStore {
 		defaults.setProperty("beagle.command",
 				"-jar ${beagle} ref=${ref} gt=${vcf} out=${prefix} nthreads=1 chrom=${chr}:${start}-${end} map=${map} impute=false");
 		defaults.setProperty("ref.fasta", "v37");
-		defaults.setProperty("contact.name", "Christian Fuchsberger");
-		defaults.setProperty("contact.email", "cfuchsb@umich.edu");
+		defaults.setProperty("contact.name", "Imputation Server Support");
+		defaults.setProperty("contact.email", "support@imputation.helmholtz-munich.de");
 		defaults.setProperty("hg38Tohg19", "chains/hg38ToHg19.over.chain.gz");
 		defaults.setProperty("hg19Tohg38", "chains/hg19ToHg38.over.chain.gz");
 		defaults.setProperty("sanitycheck", "yes");
