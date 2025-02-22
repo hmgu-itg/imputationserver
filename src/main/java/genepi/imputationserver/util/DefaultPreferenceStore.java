f702f6a116455fe5b36b1e51c0d87d448fe6f864
andrei-barysenka <andrei.barysenka@gmail.com>
2025-02-22 21:12:56 +0100

    add --chrom option to eagle command in the store

--- src/main/java/genepi/imputationserver/util/DefaultPreferenceStore.java
+++ src/main/java/genepi/imputationserver/util/DefaultPreferenceStore.java
@@ -83,7 +83,7 @@ public class DefaultPreferenceStore {
 		defaults.setProperty("minimac.command",
 				"--region ${chr}:${start}-${end} --overlap ${window} --output ${prefix}.dose.vcf.gz --output-format vcf.gz --format GT,DS,GP,HDS --min-ratio 0.00001 --decay ${decay} --temp-prefix ${minimac_temp_prefix} --temp-buffer ${minimac_temp_buffer} --all-typed-sites --sites ${prefix}.info --threads ${minimac_threads} --empirical-output ${prefix}.empiricalDose.vcf.gz ${minR2 != 0 ? '--min-r2 ' + minR2 : ''}  ${mapMinimac != null ? '--map ' + mapMinimac : ''} ${ref} ${vcf}");
 		defaults.setProperty("eagle.command",
-				"--vcfRef ${ref} --vcfTarget ${vcf} --geneticMapFile ${map} --outPrefix ${prefix} --bpStart ${start} --bpEnd ${end} --vcfOutFormat z --keepMissingPloidyX --numThreads ${eagle_threads}");
+				"--vcfRef ${ref} --vcfTarget ${vcf} --geneticMapFile ${map} --outPrefix ${prefix} --bpStart ${start} --bpEnd ${end} --vcfOutFormat z --keepMissingPloidyX --numThreads ${eagle_threads} --chrom {chrom}");
 		defaults.setProperty("beagle.command",
 				"-jar ${beagle} ref=${ref} gt=${vcf} out=${prefix} nthreads=1 chrom=${chr}:${start}-${end} map=${map} impute=false");
 		defaults.setProperty("ref.fasta", "v37");
