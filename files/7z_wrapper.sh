bbd72fe1cd1774e95fe28ef503eb7f244a18a67f
andrei-barysenka <andrei.barysenka@gmail.com>
2025-09-22 19:39:41 +0200

    add 7z wrapper

---
+++ files/7z_wrapper.sh
@@ -0,0 +1,55 @@
+#!/usr/bin/env bash
+
+# 7z wrapper
+
+SINGULARITY_EXE=""
+CONTAINER_PATH=""
+
+function usage () {
+    >&2 echo ""
+    >&2 echo "Get all run folder names, output goes to STDOUT"
+    >&2 echo ""
+    >&2 echo "Usage: $0 -p <password> -o <output.7z> -i <input1> -i <input2> ... "
+    >&2 echo ""
+    exit 0
+}
+
+# join an array
+function join_by { local IFS="$1"; shift; echo "$*"; }
+
+declare -a infnames
+outfname=""
+password=""
+while getopts "hp:o:i:" opt; do
+    case $opt in
+	p)password="$OPTARG";;
+	o)outfname="$OPTARG";;
+	i)infnames+=("$OPTARG");;
+	h)usage;;
+	*)usage;;
+    esac
+done
+shift "$((OPTIND-1))"
+
+if [[ -z "$password" ]];then
+    >&2 echo "ERROR: no password specified"
+    exit 1
+fi
+
+if [[ -z "$outfname" ]];then
+    >&2 echo "ERROR: no output file name specified"
+    exit 1
+fi
+
+if [[ "${#infnames[@]}" -eq 0 ]];then
+    >&2 echo "ERROR: no input files specified"
+    exit 1
+fi
+
+# echo $(join_by " " "${infnames[@]}")
+
+"${SINGULARITY_EXE}" exec -B /mnt/storage -B /home/hdoop/container.home/:/home/hdoop/ -B /tmp:/run/user "${CONTAINER_PATH}" 7z a -p\'"${password}"\' "-t7z" "-mhe=on" "${outfname}" $(join_by " " "${infnames[@]}")
+
+exit 0
+
+
